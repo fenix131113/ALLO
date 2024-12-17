d5e632eca5f8f7fdf53f1a3a9ca9991ee73866fb
Алексей <ix.fenix@mail.ru>
2024-12-17 13:21:34 +0300

    feat: breakable obejcts

--- ALLO/Assets/_Source/GameAssembly/DamageSystem/Data/DamageOwner.cs
+++ ALLO/Assets/_Source/GameAssembly/DamageSystem/Data/DamageOwner.cs
@@ -2,7 +2,8 @@
 {
 	public enum DamageOwner
 	{
-		PLAYER,
-		ENEMY
+		PLAYER = 0,
+		ENEMY = 1,
+		ENVIRONMENT = 2
 	}
 }
---
+++ ALLO/Assets/_Source/GameAssembly/Environment/Breakable.cs
@@ -0,0 +1,42 @@
+﻿using DamageSystem;
+using DamageSystem.Data;
+using UnityEngine;
+using Utils;
+
+namespace Environment
+{
+    public class Breakable : MonoBehaviour, IDamageable
+    {
+        private const DamageOwner DAMAGE_OWNER = DamageOwner.ENVIRONMENT;
+
+        [SerializeField] private int health;
+        [SerializeField] private int maxHealth;
+        [SerializeField] private ParticleSystem breakParticle;
+        [SerializeField] private ParticleSystem damageParticle;
+
+        public int GetHealth() => health;
+
+        public int GetMaxHealth() => maxHealth;
+
+        public DamageOwner GetOwner() => DAMAGE_OWNER;
+
+        public void TakeDamage(int damage)
+        {
+            health -= damage;
+
+            if (health <= 0)
+                DestroyObject();
+            else
+                damageParticle.Play();
+        }
+
+        private void DestroyObject()
+        {
+            breakParticle.DestroyByTime(breakParticle.main.duration);
+            breakParticle.Play();
+            breakParticle.transform.parent = null;
+            breakParticle.gameObject.transform.localScale = Vector3.one;
+            Destroy(gameObject);
+        }
+    }
+}
